f15b80c5c16164c8d691f7f57c3709e7b78f5f06
justincoh <justincoh@gmail.com>
2015-01-22 10:34:13 -0500

    Menu parsed

--- server/server.js
+++ server/server.js
@@ -8,62 +8,42 @@ Meteor.methods({
 		var meals = ["Breakfast", "Lunch", "Dinner"];
 
 		// These will be the IDs we will iterate over to get the menu items.
-		var diningHalls = ["#frank_menu", "#frary_menu", "#oldenborg_menu", "#cmc_menu", "#scripps_menu", "#pitzer_menu", "#mudd_menu"];
+		var diningHalls = ["#frank_menu","#frary_menu", "#oldenborg_menu", "#cmc_menu", "#scripps_menu", "#pitzer_menu", "#mudd_menu"];
 
     	// var table = $("#meal_header>th").text();
-    	var frank = $("#frank_menu").text();
+    	var frank = $("#frank_menu").html();
 
-    	//this is getting things roughly right
+    	
     	var menus = [];
-    	var leadingSpaces = new RegExp(/\s+/);
-    	diningHalls.forEach(function(hall){
-    		var thisMenu = $(hall).text();
-    		thisMenu = thisMenu.split('\n');
-    		thisMenu.forEach(function(ele,index){
-    			var spaceMatch = ele.match(leadingSpaces);
-    			if(spaceMatch){
-    				spaceMatch = spaceMatch[0];
-    				if(ele===spaceMatch || ele.length ===0){
-    					thisMenu.splice(index,1);
-    				}
-    			}
-    		})
-    		menus.push(thisMenu)
-    	})
-/*
-    	// Schema?
-    	{
-    		diningHallName: {
-    			date: date,
-    			breakfast: [items],
-    			lunch: [items],
-    			dinner:[items]
-    		},
-    	}
+    	
 
-		// var parseMenu = function(diningHalls) {
-		// 	diningHalls.forEach(function(hall){
-		// 	})
-		// }
-		test = frank.split('\n');
-		var leadingSpaces = new RegExp(/\s+/);
-		test.forEach(function(ele,index){
-			var spaceMatch = ele.match(leadingSpaces);
-			if(spaceMatch){
-				spaceMatch=spaceMatch[0];  //Regex always returns type array
-				if(ele===spaceMatch || ele.length ===0){  //If entry was entirely spaces, this is true
-					test.splice(index,1);
-				} 
-				else {
-					//Trim whitespace wasn't working in here, my logic is probably off somewhere
-				}
-			}
-		})
-		test.forEach(function(ele,index){
-			test[index] = ele.trim();
-		})
-		test = test.filter(function(item){return item!==''})
-*/
+    	diningHalls.forEach(function(hall){
+    		var thisMenu=$(hall).text();
+    		thisMenu = thisMenu.split('\n');
+    		
+    		console.log(thisMenu)
+    		var newMenu=[];
+    		thisMenu.forEach(function(item,index){
+    				var fixedItem = item.replace(/\t+/,'');
+    			if(fixedItem!==''){
+    				newMenu.push(fixedItem)
+    			}
+    		});
+    		menus.push(newMenu)
+    	});
+
+
+    	// Schema?
+    	// {
+    	// 	diningHallName: {
+    	// 		date: date,
+    	// 		breakfast: [items],
+    	// 		lunch: [items],
+    	// 		dinner:[items]
+    	// 	},
+    	// }
+
+
 
     return menus;
 	}
