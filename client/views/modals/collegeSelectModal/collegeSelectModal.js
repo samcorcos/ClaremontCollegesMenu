7e7121b15620ee9d73e1e1d4d1b2cda793fd34ec
Sam Corcos <samcorcos@gmail.com>
2015-01-22 10:16:37 -0500

    modal closing

--- client/views/modals/collegeSelectModal/collegeSelectModal.js
+++ client/views/modals/collegeSelectModal/collegeSelectModal.js
@@ -3,3 +3,9 @@ Template.collegeSelectModal.helpers({
     return ["Claremont McKenna", "Harvey Mudd", "Scripps", "Pitzer", "Frary", "Frank", "Oldenborg"];
   }
 })
+
+Template.collegeSelectModal.events({
+  'click a': function(e,t) {
+    IonModal.close();
+  }
+})
