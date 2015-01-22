0214b714227741110ad16b1c105a7f92ad8da020
Sam Corcos <samcorcos@gmail.com>
2015-01-22 12:41:50 -0500

    account validation successful

--- client/views/settings/settings.js
+++ client/views/settings/settings.js
@@ -7,5 +7,14 @@ Template.settings.rendered = function() {
 Template.settings.helpers({
   notifications: function() {
     return ["Item 1", "Item 2", "Item 3", "Item 4"]
+  },
+  userEmail: function() {
+    return Meteor.user().emails[0].address;
+  }
+})
+
+Template.settings.events({
+  'click #logout-button': function(e,t) {
+    Meteor.logout();
   }
 })
