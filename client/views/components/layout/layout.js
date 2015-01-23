63146d7eac369bda644e0cc2dea1184372154687
Sam Corcos <samcorcos@gmail.com>
2015-01-23 12:20:15 -0500

    home routes to favorite, modal on button push -iphone only-

--- client/views/components/layout/layout.js
+++ client/views/components/layout/layout.js
@@ -1,3 +1,10 @@
 Template.layout.events({
-  
+  'click .ion-ios-book': function(e,t) {
+    IonModal.open("collegeSelectModal")
+  },
+  'click .ion-ios-book-outline': function(e,t) {
+    var fav = Meteor.user().profile.favorite;
+    console.log("THIS IS FAV", fav);
+    Router.go("/"+fav)
+  }
 })
---
+++ server/editUser.js
@@ -0,0 +1,5 @@
+Meteor.methods({
+  changeFav: function(newFav) {
+    // This is where we change the current user's favorite in his profile
+  }
+})
