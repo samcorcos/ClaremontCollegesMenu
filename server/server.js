2fda385a21a02eca3e62bd391995d6694e7002c8
justincoh <justincoh@gmail.com>
2015-01-23 12:14:05 -0500

    Date history working, need momentjs still

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ Meteor.methods({
 	getMenus: function(){
 		var results = Meteor.http.get("https://aspc.pomona.edu/menu/", {timeout: 30000});
 		var html = results.content;
-		$ = cheerio.load(html)
+		$ = cheerio.load(html);
 
 		var halls ={
 			"#frank_menu" : "Frank",
@@ -32,19 +32,18 @@ Meteor.methods({
 				}
 			});
 
-			console.log('NEW MENU ',newMenu)
 			var breakfastIndex = newMenu.indexOf('Breakfast');
 			var lunchIndex = newMenu.indexOf('Lunch');
 			var dinnerIndex = newMenu.indexOf('Dinner');
-			var breakfast = newMenu.slice(breakfastIndex+1,lunchIndex)
-			var lunch = newMenu.slice(lunchIndex+1,dinnerIndex)
-			var dinner = newMenu.slice(dinnerIndex+1)
+			var breakfast = newMenu.slice(breakfastIndex+1,lunchIndex);
+			var lunch = newMenu.slice(lunchIndex+1,dinnerIndex);
+			var dinner = newMenu.slice(dinnerIndex+1);
 			menus.push({
 				hall:halls[hall],
 				breakfast:breakfast,
 				lunch:lunch,
 				dinner:dinner
-			})
+			});
 		});
 
 		populateCollections(menus);
@@ -59,55 +58,39 @@ Meteor.methods({
 
 var populateCollections = function(arrayOfMenuObjects){
 	var menus= arrayOfMenuObjects;
-
+	var today = new Date();
 	menus.forEach(function(menuObject){
 		menuObject.breakfast.forEach(function(item){
-			var found = MenuItems.findOne({itemName:item,college:menuObject.hall,meal:'Breakfast'});	
-			if(!found){
-				MenuItems.insert({
-					itemName:item,
-					college:menuObject.hall,
-					meal:'Breakfast'
-				},function(err,res){
-					if(err){console.log(err)}
-				})
-			}
+			MenuItems.upsert({itemName:item,college:menuObject.hall,meal:'Breakfast'},
+				{	
+					$addToSet: {dateHistory: today},
+					$set:{date:today}
+				}
+			);
 		});
 		menuObject.lunch.forEach(function(item){
-			var found = MenuItems.findOne({itemName:item,college:menuObject.hall,meal:'Lunch'})
-			if(!found){
-				MenuItems.insert({
-					itemName:item,
-					college:menuObject.hall,
-					meal:'Lunch'
-				},function(err,res){
-					if(err){console.log(err)}
-						
-				})
-			}
+			MenuItems.upsert({itemName:item,college:menuObject.hall,meal:'Lunch'},
+				{	
+					$addToSet: {dateHistory: today},
+					$set:{date:today}
+				}
+			);
 		});
 		menuObject.dinner.forEach(function(item){
-			var found = MenuItems.findOne({itemName:item,college:menuObject.hall,meal:'Dinner'})
-			if(!found){
-				MenuItems.insert({
-					itemName:item,
-					college:menuObject.hall,
-					meal:'Dinner'
-				},function(err,res){
-					if(err){console.log(err)}
-						
-				})
-			}
+			MenuItems.upsert({itemName:item,college:menuObject.hall,meal:'Dinner'},
+				{	
+					$addToSet: {dateHistory: today},
+					$set:{date:today}
+				}
+			);
 		});
-	})
+	});
 };
 
 var fillTodaysMenu = function(arrayOfMenuObjects){
 	TodaysMenu.remove({});
 	var menus = arrayOfMenuObjects;
-	// var todaysMenu = [];
 	menus.forEach(function(menuObject){
-		// todaysMenu[menuObject.hall] = {breakfast:[],lunch:[],dinner:[]};
 		menuObject.breakfast.forEach(function(item){
 			var fetched = MenuItems.findOne({itemName:item,college:menuObject.hall,meal:'Breakfast'});
 			TodaysMenu.insert(fetched)
