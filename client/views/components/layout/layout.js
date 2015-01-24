7ef159a63572e09e1681abd17d4b95aabc128952
Sam Corcos <samcorcos@gmail.com>
2015-01-23 19:52:53 -0500

    routing buttons working

--- client/views/components/layout/layout.js
+++ client/views/components/layout/layout.js
@@ -1,11 +1,34 @@
 Template.layout.events({
-  'click .ion-ios-book': function(e,t) {
-    IonModal.open("collegeSelectModal")
+  'click #toggle-left-button':function(e,t) {
+    
+    // Need to figure out how to get this transition to go the right way...
+
+    var collegeName = $("#college-name").text().toLowerCase();
+    var collegeList = ["cmc", "mudd", "scripps", "pitzer", "frary", "frank", "oldenborg"];
+
+    var currIndex = collegeList.indexOf(collegeName);
+
+    if (currIndex === 0) {
+      Router.go("/oldenborg")
+    } else {
+      var newIndex = currIndex - 1;
+      var newRoute = collegeList[newIndex];
+      Router.go("/"+newRoute)
+    }
   },
-  'click .ion-ios-book-outline': function(e,t) {
-    // var fav = Meteor.user().profile.favorite;
-    // Router.go("/");
-    // Router.go("/");
-    // IonModal.open("collegeSelectModal")
+  'click #toggle-right-button': function(e,t) {
+
+    var collegeName = $("#college-name").text().toLowerCase();
+    var collegeList = ["cmc", "mudd", "scripps", "pitzer", "frary", "frank", "oldenborg"];
+
+    var currIndex = collegeList.indexOf(collegeName);
+
+    if (currIndex === 6) {
+      Router.go("/cmc")
+    } else {
+      var newIndex = currIndex + 1;
+      var newRoute = collegeList[newIndex];
+      Router.go("/"+newRoute)
+    }
   }
 })
