4a2fa71da0b10a5f33700c7ee037a466636d9475
Sam Corcos <samcorcos@gmail.com>
2015-02-09 12:35:12 -0500

    adding mobile config file

---
+++ mobile-config.js
@@ -0,0 +1,16 @@
+App.info({
+  name: 'Claremont Colleges Menu',
+  description: 'Daily menu for the Claremont Colleges',
+  version: '1.0.0'
+});
+
+App.icons({
+  'iphone': 'resources/icons/icon-60.png',
+  'iphone_2x': 'resources/icons/icon-60@2x.png'
+});
+
+App.launchScreens({
+  'iphone': 'resources/splash/Default~iphone.png',
+  'iphone_2x': 'resources/splash/Default@2x~iphone.png',
+  'iphone5': 'resources/splash/Default-568h@2x~iphone.png'
+});
